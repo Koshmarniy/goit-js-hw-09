bcfe10e2842922865aae58ca77562189137d3b3b
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2023-01-06 10:38:14 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,4 +7,20 @@ let timerId = null;
 btnStart.addEventListener('click', onStartClik);
 btnStop.addEventListener('click', onStopClick);
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
+function onStartClik() {
+    timerId = setInterval(()=>{
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;  
+    }, 1000);
+    btnStart.disabled = true;
+
+};
+
+function onStopClick() {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+}

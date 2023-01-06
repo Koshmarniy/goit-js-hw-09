944240fd9009fb81a240f7df9a610e4ca45f79ff
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2023-01-06 10:13:38 +0200

    add some code task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,10 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+btnStart.addEventListener('click', onStartClik);
+btnStop.addEventListener('click', onStopClick);
+
 

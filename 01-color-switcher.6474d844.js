e5131f43b2e6c7e3d49a7372636b551136b2f4c6
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2023-01-06 08:38:43 +0000

    Deploying to gh-pages from @ Koshmarniy/goit-js-hw-09@bcfe10e2842922865aae58ca77562189137d3b3b 🚀

--- 01-color-switcher.6474d844.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.querySelector("body");t.addEventListener("click",onStartClik),e.addEventListener("click",onStopClick)}();
-//# sourceMappingURL=01-color-switcher.6474d844.js.map

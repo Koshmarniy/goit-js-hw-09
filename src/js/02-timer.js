710b78ff5baade62673eb25c7afd536be7b791df
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2023-01-08 13:24:52 +0200

    fineshed task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,16 @@
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
-// all modules
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const refs = {
-    input: document.querySelector('#datetime-picker'),
-    btn: document.querySelector('[data-start]'),
-
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-    seconds: document.querySelector('data-seconds'),
+  input: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('[data-start]'),
+  // Interface Watchface
+  days: document.querySelector('[data-days'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
 let intervalId = null;
@@ -21,34 +19,88 @@ let endTime = null;
 refs.btn.addEventListener('click', onStartBtn);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-    //   console.log(selectedDates[0]);
-      const currentTime = Date.now();
-      const isPastDate = selectedDates[0] < currentTime;
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    // console.log(selectedDates[0]);
+    const currentTime = Date.now();
+    const isPastDate = selectedDates[0] < currentTime;
 
-      if (isPastDate) {
-        Notiflix.Notify.failure('Please choose a date in the future');
-        init();
-        clearInterval(intervalId);
-        return
-      }
+    if (isPastDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      init();
+      clearInterval(intervalId);
+      return;
+    }
 
-      refs.btn.disabled = false;
-      endTime = selectedDates[0];
+    refs.btn.disabled = false;
+    endTime = selectedDates[0];
 
-      updateWatchface(countDeltaTime());
+    updateWatchface(countDeltaTime());
+  },
+};
 
-    },
-  };
+flatpickr(refs.input, options);
 
-  flatpickr(refs.input, options);
+init();
 
-  init();
+function init() {
+  refs.btn.disabled = true;
+  updateWatchface(0);
+}
 
-  function init() {
-    
+function onStartBtn() {
+  intervalId = setInterval(onTick, 1000);
+}
+
+function onTick() {
+  const deltaTime = countDeltaTime();
+  const isEndTimer = deltaTime < 0;
+
+  if (isEndTimer) {
+    clearInterval(intervalId);
+    Notiflix.Notify.success('Greeting, it is finish.');
+    return;
   }
+
+  updateWatchface(deltaTime);
+}
+
+function countDeltaTime() {
+  const currentTime = Date.now();
+  return endTime - currentTime;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateWatchface(deltaTime) {
+  const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
